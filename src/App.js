73a2e4634d0709c885099cba5759226c04f24263
Femosky <olufemiojeyemi@gmail.com>
2023-10-09 10:34:15 +0100

    Module 12 part 1 (Custom Hooks) done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-import { Form } from './components/Form';
+import { useToggle } from './useToggle';
 
 function App() {
+    const { state: isVisible, toggle } = useToggle(false);
+    const { state: isVisible2, toggle: toggle2 } = useToggle(false);
+
     return (
         <div className="App">
-            <Form />
+            <button onClick={toggle}>{isVisible ? 'Hide' : 'Show'}</button>
+            {isVisible && <h1>Hidden Text</h1>}
+            <button onClick={toggle2}>{isVisible ? 'Hide' : 'Show'}</button>
+            {isVisible2 && <h1>Hidden Text</h1>}
         </div>
     );
 }
---
+++ src/useToggle.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export function useToggle(initialVal = false) {
+    const [state, setState] = useState(initialVal);
+
+    function toggle() {
+        setState((prev) => !prev);
+    }
+
+    return { state, toggle };
+}
